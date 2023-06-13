ddbbcd3047a09af3eef041ca20e2b7d38cc1042e
Kitaev_Dmitry <kitaevdg@yandex.ru>
2023-06-13 18:00:52 +0300

    Переменные

--- sandbox/src/main/java/kit/sandbox/firstJavaProgram.java
+++ sandbox/src/main/java/kit/sandbox/firstJavaProgram.java
@@ -5,12 +5,16 @@ public class firstJavaProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 
-		System.out.println(21 / 3.0);
-		System.out.println(21 - 14);
+		int sideSqare = 21;
+		int sqareArea = (sideSqare * sideSqare);
+		System.out.println(sideSqare / 3.0);
+		System.out.println(sideSqare - 14);
 		System.out.println(3.0 + 4);
 		System.out.println((5.2 + 1.8) * 7);
 
 		System.out.println("7" + "7");
 		System.out.println("7" + 7);
+		System.out.println("Sqare area is " + sqareArea);
+		System.out.println("Привет!");
 	}
 }
