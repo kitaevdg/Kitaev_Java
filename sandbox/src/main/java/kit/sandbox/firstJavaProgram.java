da063ab0d7ed50b9f28cd5e1988575106581c8b4
Kitaev_Dmitry <kitaevdg@yandex.ru>
2023-06-13 18:56:08 +0300

    Создание  и использование переменных int, double, String

--- sandbox/src/main/java/kit/sandbox/firstJavaProgram.java
+++ sandbox/src/main/java/kit/sandbox/firstJavaProgram.java
@@ -3,18 +3,12 @@ package kit.sandbox;
 public class firstJavaProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		String everyWhere = "world";
+		System.out.println("Hello " + everyWhere + "!");
 
-		int sideSqare = 21;
-		int sqareArea = (sideSqare * sideSqare);
+		double sideSqare = 21;
+		double sqareArea = (sideSqare * sideSqare);
+		System.out.println("Sqare area is " + sideSqare + " * " + sideSqare + " = " + sqareArea);
 		System.out.println(sideSqare / 3.0);
-		System.out.println(sideSqare - 14);
-		System.out.println(3.0 + 4);
-		System.out.println((5.2 + 1.8) * 7);
-
-		System.out.println("7" + "7");
-		System.out.println("7" + 7);
-		System.out.println("Sqare area is " + sqareArea);
-		System.out.println("Привет!");
 	}
 }
