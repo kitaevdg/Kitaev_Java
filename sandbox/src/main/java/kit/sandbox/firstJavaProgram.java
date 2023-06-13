731da91a3724d28e8317dc7697af17f83cf3735e
Kitaev_Dmitry <kitaevdg@yandex.ru>
2023-06-13 12:51:55 +0300

    Арифметика и склейка (контетенация)

--- sandbox/src/main/java/kit/sandbox/firstJavaProgram.java
+++ sandbox/src/main/java/kit/sandbox/firstJavaProgram.java
@@ -4,5 +4,13 @@ public class firstJavaProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+
+		System.out.println(21 / 3.0);
+		System.out.println(21 - 14);
+		System.out.println(3.0 + 4);
+		System.out.println((5.2 + 1.8) * 7);
+
+		System.out.println("7" + "7");
+
 	}
 }
