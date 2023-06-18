8af44ee9ff07eefaf96f0925a06112d95ddd9123
Kitaev_Dmitry <kitaevdg@yandex.ru>
2023-06-19 00:21:45 +0300

    Добавлены функции

--- sandbox/src/main/java/kit/sandbox/firstJavaProgram.java
+++ sandbox/src/main/java/kit/sandbox/firstJavaProgram.java
@@ -2,13 +2,34 @@ package kit.sandbox;
 
 public class firstJavaProgram {
 
-	public static void main(String[] args) {
-		String everyWhere = "world";
-		System.out.println("Hello " + everyWhere + "!");
+  public static void main(String[] args) {
+    helloFunction("RockStar:)");
+    
+    double leningrad = 7;
+    System.out.println("Square area is " + leningrad + " * " + leningrad + " = " + area(leningrad));
 
-		double sideSqare = 21;
-		double sqareArea = (sideSqare * sideSqare);
-		System.out.println("Sqare area is " + sideSqare + " * " + sideSqare + " = " + sqareArea);
-		System.out.println(sideSqare / 3.0);
-	}
+    double a = 77;
+    double b = 7;
+    System.out.println("Rectangle area is " + a + " * " + b + " = " + area(a, b));
+
+    String everyWhere = "world!";
+    System.out.println("Hello " + everyWhere + "!!");
+
+    double sideSquare = 21;
+    double squareArea = (sideSquare * sideSquare);
+    System.out.println("Square area is " + sideSquare + " * " + sideSquare + " = " + squareArea);
+    System.out.println(sideSquare / 3.0);
+  }
+
+  public static void helloFunction(String everyBody) {
+    System.out.println("Hello, " + everyBody + " It's function!");
+  }
+
+  public static double area(double l) { // здесь мы определили функцию area для площади квадрата;
+    return l * l; // а здесь определили, что именно  функция area должна возвращать;
+  }
+
+  public static double area(double a, double b) { // здесь мы определили функцию area для площади квадрата;
+    return a * b; // а здесь определили, что именно  функция area должна возвращать;
+  }
 }
